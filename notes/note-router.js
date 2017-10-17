a0dee4ada3a7d9464594213479512544bc25223e
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-17 16:12:24 -0700

    get and posts fully working

--- notes/note-router.js
+++ notes/note-router.js
@@ -12,7 +12,7 @@ router.post('/api/notes', (req,res) => {
     
     let data = {};
     data[note.id] = note;
-    let saveNote = JSON.stringify(note);
+    let saveNote = JSON.stringify(data);
 
     fs.outputFile(database, saveNote)
       .then(response.sendJSON(res, 200, note))
@@ -24,12 +24,32 @@ router.post('/api/notes', (req,res) => {
 
 router.get('/api/notes', (req,res) => {
   if(req.url.query.id){
-    response.sendStatus(res,200,'you gave an id');
+    let id = req.url.query.id;
+    fs.readJson(database)
+      .then(notes => {
+        let note = notes[id];
+        response.sendJSON(res, 200, note);
+      })
+      .catch(err => response.sendStatus(res, 400, 'id does not exist'));
   } else{
     fs.readJson(database)
       .then(notes => response.sendJSON(res, 200, notes))
-      .catch(err => response.sendStatus(res, 400, err));
+      .catch(err => response.sendStatus(res, 400, 'error reading JSON'));
   }
-
 });
  
+router.delete('/api/notes', (req,res) => {
+  if(req.url.query.id){
+    let id = req.url.query.id;
+    fs.readJson(database)
+      .then(notes => {
+        delete notes[id];
+        response.sendJSON(res, 200, 'note deleted');
+      })
+      .catch(err => response.sendStatus(res, 400, 'derp!'));
+  } else{
+    fs.readJson(database)
+      .then(notes => response.sendJSON(res, 200, notes))
+      .catch(err => response.sendStatus(res, 400, 'error reading JSON'));
+  }
+});
