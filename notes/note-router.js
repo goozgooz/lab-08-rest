d0b6655919c14a474b9405e614ec13aeb743145f
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-19 15:53:59 -0700

    i hate bugs

--- notes/note-router.js
+++ notes/note-router.js
@@ -4,28 +4,31 @@ const router = require('../lib/router.js');
 const Note = require('./model.js');
 const response = require('../lib/response.js');
 const database = __dirname + '/../data/notes.dat';
-const storage = require('../lib/storage')(database);
+const storage = require('../lib/storage.js')(database);
 
 router.post('/api/notes', (req,res) => {
   
   let note = new Note(req.body);
+  console.log(note);
   storage.saveItem(note)
     .then(notes => response.sendJSON(res, 200, notes))
     .catch(err => response.sendStatus(res,400, 'poorly formatted JSON'));
 });
 
 router.get('/api/notes', (req,res) => {
-  // if(req.url.query.id){
-  //   let id = req.url.query.id;
-  //   fs.readJson(database
-  //     .catch(err => response.sendStatus(res, 400, 'id does not exist'));
-  // } else{
+
+  let id = req.url && req.url.query && req.url.query.id;
+
+  if(id) {
+    // print specific note out
+  } else {
     storage.getItems()
-      .then(console.log('hi'))
-      .catch(console.log('bye'))
-      // .then(notes => response.sendJSON(res, 200, notes))
-      // .catch(err => response.sendStatus(res, 400, 'nothing in database'));
-  // };
+      .then(allNotes => {
+        console.log(allNotes);
+        response.sendJSON(res, 200, allNotes);
+      })
+      .catch( () => response.sendStatus(res, 400, 'nothing in database'));
+  }
 });
  
 // router.delete('/api/notes', (req,res) => {
